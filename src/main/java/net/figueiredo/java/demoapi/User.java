3fb268862a68ee8d7f5ac099c63e176da2bd934d
Carlos Figueiredo <carlosfigueiredo@souunisuam.com.br>
2023-06-13 21:42:48 -0300

    2023.06.13
    teste com o User.

---
+++ src/main/java/net/figueiredo/java/demoapi/User.java
@@ -0,0 +1,38 @@
+package net.figueiredo.java.demoapi;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(nullable = false)
+	private Long id;
+	
+	@Column(length = 127, nullable = false)
+	private String name;
+	
+	
+	@Column(length = 255)
+	private String email;
+	
+	@Column(length = 63)
+	private String password;
+	
+	
+}
+
